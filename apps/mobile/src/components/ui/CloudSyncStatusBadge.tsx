370f1dddeec10121a1cedbee65867da9e53b42c6
Angel <angel@example.com>
2026-08-30 16:20:57 +0200

    fix(ui): correct theme imports in CloudSyncStatusBadge

--- apps/mobile/src/components/ui/CloudSyncStatusBadge.tsx
+++ apps/mobile/src/components/ui/CloudSyncStatusBadge.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useDev } from '../../context/DevContext';
-import { Colors } from '../../constants/colors';
-import { Typography } from '../../constants/typography';
+import { Colors } from '../../theme/colors';
+import { Typography } from '../../theme/tokens';
 import { triggerHaptic } from '../../utils/haptics';
 
 interface CloudSyncStatusBadgeProps {
